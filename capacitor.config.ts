02d446cfd391dc1409feb825d18b358e2ace5d5b
Rahul Singh <rahulmeyok558@gmail.com>
2026-07-13 13:52:58 +0530

    Convert Capacitor to Hosted Web App wrapper to solve all CORS and OAuth redirect bugs

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.flatsplit.app',
   appName: 'FlatSplit',
-  webDir: 'out'
+  server: {
+    url: 'https://flatsplit.meals4heal.in',
+    cleartext: true
+  }
 };
 
 export default config;
